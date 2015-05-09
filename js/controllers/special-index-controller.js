7c6ca0858906f5aa67824fe0bcfe0951b3239cb2
atriatech94 <hamid_7555@hotmail.com>
2015-05-09 14:42:49 +0430

    n16

--- js/controllers/special-index-controller.js
+++ js/controllers/special-index-controller.js
@@ -2,6 +2,32 @@ angular.module('shoooma')
 .controller('SpecialIndexController', function($scope,$rootScope) {
 
     $scope.rent = $rootScope.special;
+    console.log($rootScope.special);
 
+})
+.directive('swFancy2', function (){
+		return {
+			link: function($scope) {
+             
+                    $( ".contain" ).delegate( ".special_one", "click", function() {
+                        var amin = $scope.special;
+                        console.log(amin);
+                        melk_id =  $(this).attr('melk_id');
+                        var groups = $.grep(amin, function(e){ return e.id == melk_id });
+                        var content = '<div class="melks_detail">';
+                        content += '<h2>'+groups[0].title+'</h2><br>' ;
+                        content += '<img width="90" src="http://www.shoooma.com/admin/include/upload/special/'+groups[0].picname+'">' ;
+                        content += '<br><div class="m_detail">'+groups[0].description+'</div>' ;
+                        content += '<div>' ;
+                        
+                        $.fancybox.open( content,{
+                            padding : 20
+                        });
+                    });
+                    
+                     
+			},//end link
+        }
 });
+
 
